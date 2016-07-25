a180008cf4e5949abeb785aa583772e838ce9363
Guilherme Henrique <gjhenrique@gmail.com>
2016-07-24 22:56:28 -0300

    Expand parent of the hour of day

--- app/src/main/java/br/uel/easymenu/gui/MultiMealFragment.java
+++ app/src/main/java/br/uel/easymenu/gui/MultiMealFragment.java
@@ -9,12 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Calendar;
 import java.util.List;
 
+import javax.inject.Inject;
+
+import br.uel.easymenu.App;
 import br.uel.easymenu.R;
 import br.uel.easymenu.adapter.LastDishDivider;
 import br.uel.easymenu.adapter.MealListAdapter;
 import br.uel.easymenu.model.Meal;
+import br.uel.easymenu.service.MealService;
 
 public class MultiMealFragment extends Fragment {
 
@@ -22,8 +27,13 @@ public class MultiMealFragment extends Fragment {
 
     private MealListAdapter listAdapter;
 
+    @Inject
+    protected MealService mealService;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ((App) this.getContext().getApplicationContext()).component().inject(this);
+
         List<Meal> meals = getArguments().getParcelableArrayList(MEAL_BUNDLE);
 
         View expandableListLayout = inflater.inflate(R.layout.expandable_listview, container, false);
@@ -32,6 +42,9 @@ public class MultiMealFragment extends Fragment {
         recyclerView.setAdapter(listAdapter);
         recyclerView.addItemDecoration(new LastDishDivider(this.getContext()));
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        int index = mealService.selectMealByTimeIndex(meals, Calendar.getInstance());
+        listAdapter.expandParent(index);
 
         return expandableListLayout;
     }
--- app/src/main/java/br/uel/easymenu/ioc/AppComponent.java
+++ app/src/main/java/br/uel/easymenu/ioc/AppComponent.java
@@ -2,6 +2,7 @@ package br.uel.easymenu.ioc;
 
 import br.uel.easymenu.gui.MainActivity;
 import br.uel.easymenu.gui.MenuActivity;
+import br.uel.easymenu.gui.MultiMealFragment;
 import dagger.Component;
 
 @Component(modules = AppModule.class)
@@ -9,4 +10,6 @@ public interface AppComponent {
     void inject(MainActivity mainActivity);
 
     void inject(MenuActivity menuActivity);
+
+    void inject(MultiMealFragment fragment);
 }
