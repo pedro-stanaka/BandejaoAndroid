cf837329c3eaa701c845d73cc6d7386e0021be05
Guilherme Henrique <gjhenrique@gmail.com>
2016-07-24 22:56:29 -0300

    New structure of overlays
    Add Screenshots photo

--- app/src/main/java/br/uel/easymenu/gui/MenuActivity.java
+++ app/src/main/java/br/uel/easymenu/gui/MenuActivity.java
@@ -256,7 +256,6 @@ public class MenuActivity extends AppCompatActivity implements NavigationView.On
         } else {
             title = currentUniversity.getFullName();
         }
-        title = "University of Cambridge";
         View headerView = navigationView.getHeaderView(0);
         TextView titleTextView = (TextView) headerView.findViewById(R.id.title_drawer);
         titleTextView.setText(title);
--- app/src/overlay/generator/gimp-script.scm
+++ app/src/overlay/generator/gimp-script.scm
@@ -1,17 +1,35 @@
 (define background-color '(221 81 76))
+; Used in drawer screenshot background
+(define dark-background-color '(88 32 30))
 (define fork-color '(0 0 0))
+
+(define (index list position i)
+  (if (= position i)
+      (car list)
+      (index (cdr list) position (+ i 1))))
 
 (define (process-image base-image
                        primary-color
                        secondary-color
                        dimension
                        path)
+  (gimp-message (car path))
   (let* ((image (car (gimp-file-load RUN-NONINTERACTIVE base-image base-image)))
-         (drawable (car (gimp-image-get-active-drawable image))))
-    (paint-image image drawable background-color primary-color)
-    (paint-image image drawable fork-color secondary-color)
-    (scale-image dimension image)
-    (write-png path image drawable)))
+         (drawable (car (gimp-image-get-active-drawable image)))
+         (list-path (map cons path dimension))
+         (i 0))
+    (while (< i (length list-path))
+           (let* ((elem (index list-path i 0))
+                  (path (car elem))
+                  (dimension (cdr elem)))
+           (paint-image image drawable background-color primary-color)
+           (paint-image image drawable dark-background-color primary-color)
+           (if (not (null? secondary-color))
+               (paint-image image drawable fork-color secondary-color))
+           (scale-image dimension image)
+           (write-png path image drawable)
+           (set! i (+ i 1))))))
+
 
 (define (paint-image image drawable from-color to-color)
     (gimp-image-select-color image CHANNEL-OP-REPLACE drawable from-color)
